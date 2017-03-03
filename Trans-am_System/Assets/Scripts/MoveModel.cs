877afb4788ce1398f8bfe9b88a43db82352969fb
Keita Marumoto <marumoto.keita@gmail.com>
2017-03-03 10:07:07 +0900

    フォルダ追加。および、プレイヤー相当のモデルをマウス追従させるスクリプト追加。
    Resourcesフォルダ追加。
    Scriptsフォルダ追加。
    MoveModel.cs追加。これはオブジェクトをマウス追従させるものである。

---
+++ Trans-am_System/Assets/Scripts/MoveModel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MoveModel : MonoBehaviour {
+	Vector3 mousePosition;
+	Vector3 worldPoint;
+	void Update()
+	{
+		mousePosition = Input.mousePosition;
+		mousePosition.z = 10;
+		//Debug.Log(mousePosition);
+
+		worldPoint = Camera.main.ScreenToWorldPoint(mousePosition);
+		//Debug.Log(worldPoint);
+		worldPoint.z = 0;
+		transform.position = worldPoint;
+	}
+}
